1248eb4f13d044983e49f19c8f4119363441587c
kull <baguvix.montoya@gmail.com>
2024-07-27 00:48:15 -0600

    change base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const BASE_PATH = '/Cuestionario('
+const BASE_PATH = '/Cuestionario/'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
